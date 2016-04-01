182486edac036a36bb468999065d6d533673f60b
AbhyudayaChatterjee <chatterjeeabhyudaya@gmail.com>
2016-04-01 21:54:57 +0530

    Files added

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-type: application/json');
+	$status = array(
+		'type'=>'success',
+		'message'=>'Thank you for contact us. As early as possible  we will contact you '
+	);
+ $name=$_REQUEST['name']; 
+$email = $_REQUEST['email'];
+$subject = "Contact from Website";
+$message = $_REQUEST['message'];
+ if (($name=="")||($email=="")||($message=="")) 
+        { 
+        echo "All fields are required, please fill <a href=\"\">the form</a> again."; 
+		die;
+        } 
+    else{         
+        $from="From: $name<$email>\r\nReturn-path: $email"; 
+        $subject="Message sent using your contact form"; 
+        mail("mail2msitgnosis@gmail.com", $subject, $message, $email);
+		echo json_encode($status);
+		die;
+        } 
+    }
+?>
